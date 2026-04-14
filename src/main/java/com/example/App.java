f1eebbeb15a50b5c21798cc4f4736454f7492546
shreya20m <shreyamehtry20@gmail.com>
2026-04-14 19:48:15 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,32 +5,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App 
-{
-    public static void main(String[] args) throws InterruptedException
-    {
-        // 🔥 IMPORTANT: Headless setup for Jenkins
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // run without UI
+
+        options.addArguments("--headless=new"); // IMPORTANT
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*"); // FIX crash
+        options.addArguments("--disable-gpu");
 
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
-        Thread.sleep(2000);
+
+        driver.manage().window().maximize();
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        Thread.sleep(2000);
-
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        Thread.sleep(2000);
-
         driver.findElement(By.id("login-button")).click();
-        Thread.sleep(2000);
 
-        System.out.println("Login successful!");
-        
-        driver.quit(); // 🔥 IMPORTANT (close browser properly)
+        Thread.sleep(3000);
+
+        System.out.println("Login Successful");
+
+        driver.quit();
     }
 }
