f9840b7f3850cd3f2923241e51882fbcaef33914
shreya20m <shreyamehtry20@gmail.com>
2026-04-14 19:27:54 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,18 +3,21 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App 
 {
     public static void main(String[] args) throws InterruptedException
     {
-        WebDriver driver = new ChromeDriver();
-        Thread.sleep(2000);
+        // 🔥 IMPORTANT: Headless setup for Jenkins
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // run without UI
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
-        Thread.sleep(2000);
-
-        driver.manage().window().maximize();
         Thread.sleep(2000);
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -25,5 +28,9 @@ public class App
 
         driver.findElement(By.id("login-button")).click();
         Thread.sleep(2000);
+
+        System.out.println("Login successful!");
+        
+        driver.quit(); // 🔥 IMPORTANT (close browser properly)
     }
 }
